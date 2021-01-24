192bdd31beddbc6cefd96cb99871e946e77e5b90
MurariK <krishna.murari@broadridge.com>
2021-01-24 18:44:14 +0530

    Main

--- src/main.py
+++ src/main.py
@@ -82,18 +82,18 @@ class BMI:
         return self.df
 
 
-if __name__== '__main__':
-    data = [
-                {"Gender":"Male","HeightCm":171,"WeightKg":96},
-                {"Gender":"Male","HeightCm":161,"WeightKg":85},
-                {"Gender":"Male","HeightCm":180,"WeightKg":77},
-                {"Gender":"Female","HeightCm":166,"WeightKg":62},
-                {"Gender":"Female","HeightCm":150,"WeightKg":70},
-                {"Gender":"Female","HeightCm":167,"WeightKg":82},
-            ]
+# if __name__== '__main__':
+#     data = [
+#                 {"Gender":"Male","HeightCm":171,"WeightKg":96},
+#                 {"Gender":"Male","HeightCm":161,"WeightKg":85},
+#                 {"Gender":"Male","HeightCm":180,"WeightKg":77},
+#                 {"Gender":"Female","HeightCm":166,"WeightKg":62},
+#                 {"Gender":"Female","HeightCm":150,"WeightKg":70},
+#                 {"Gender":"Female","HeightCm":167,"WeightKg":82},
+#             ]
 
-    df_ = pd.DataFrame(data)
-    bmi_ = BMI(df_)
-    print(bmi_.get_overweight_count())
-    print(bmi_.get_bmi_report().columns)
- 
+#     df_ = pd.DataFrame(data)
+#     bmi_ = BMI(df_)
+#     print(bmi_.get_overweight_count())
+#     print(bmi_.get_bmi_report().columns)
+#     print(list(BMI.bmi_chart(10.29)))
